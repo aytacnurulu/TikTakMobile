f69ca4f2fed717d422dad511a80e06ccc8e7125c
aytacnurulu <aytacnurulu332@gmail.com>
2026-07-30 23:54:24 +0400

    chore: update dependencies and add new packages
    - Added new dependencies: @gorhom/bottom-sheet, @react-navigation libraries, @tanstack/react-query, axios, dayjs, formik, i18next, react-i18next, react-native-gesture-handler, react-native-mmkv, react-native-reanimated, react-native-screens, react-native-vector-icons, yup, and zustand.
    - Updated react-native-safe-area-context to version 5.8.0.
    - Added dev dependency @types/react-native-vector-icons and babel-plugin-module-resolver.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    // digər plugin-lər (module-resolver daxil) əvvəl
+    'react-native-reanimated/plugin', // mütləq sonuncu
+  ],
 };
